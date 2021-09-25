285119180358a9659c2ec0b14064fd6316502010
Sawyer Cherry <sawyeracherry@gmail.com>
2021-09-24 19:24:57 -0500

    first commit

---
+++ CryptoAlarm/CryptoAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAlarmApp.swift
+//  CryptoAlarm
+//
+//  Created by Sawyer Cherry on 9/24/21.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
